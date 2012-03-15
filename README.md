4db27992e1b09bd43e6cc0a0f56d293cea9b0b0a
Peter Berkenbosch <peter@pero-ict.nl>
2012-03-15 12:23:03 +0100

    created skeleton rails 3.1.4 and spree 1.0.2 shop

---
+++ README.md
@@ -0,0 +1,11 @@
+# SpreeCasts Episode #4: Using Static Content Extension
+
+Setup sample store: 
+
+```
+gem install rails -v=3.1.4
+gem install spree
+rails _3.1.4_ new episode-4
+cd episode-4
+spree install --auto-accept
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Episode4::Application.config.secret_token = 'ef63fdaedcfd4da4672a8cf1e5e4738f324bf68b53d08d79000653c3bf50d267ae2a28e877d59a933ec5c5c6a3fe8a42161498adc6040aca40b93955055fc68e'
